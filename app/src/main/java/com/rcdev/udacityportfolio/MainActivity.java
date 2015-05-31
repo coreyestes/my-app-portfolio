e8700139991295d1c6f3d96680e55daf9d622241
root <root@ip-10-47-174-141.ec2.internal>
2015-05-31 00:15:07 +0000

    Committed by Udacity

---
+++ app/src/main/java/com/rcdev/udacityportfolio/MainActivity.java
@@ -0,0 +1,94 @@
+package com.rcdev.udacityportfolio;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void launchCapstone(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Capstone app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchXyz(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch XYZ app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchBuildIt(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Build it app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchLibrary(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Library app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchScores(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Scores app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void launchStreamer(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Streamer app ";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
